44db0eff49da643c2dec1eefe3268beecab47a2d
Lecture-Plus <Lecture-Plus@users.noreply.github.com>
2026-06-14 03:35:29 +0200

    feat: SVG art gen\u00e9r\u00e9 pour Cyber (circuit board), Street Art (graffiti tags + beziers), Synthwave (soleil r\u00e9trowave + grille perspective) + fix contraste texte btn

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker – Compta+
 // Stratégie : Network First pour l'app shell (auto-update), Cache pour CDN
 
-const CACHE_NAME = 'compta-plus-cb430710';
+const CACHE_NAME = 'compta-plus-298c3877';
 
 const APP_SHELL = [
   './index.html',
